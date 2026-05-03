cea887c79cf418459c7c1c94a5f9b532066209f9
Sukanya Sudhir Shete <47360797+sukanyashete@users.noreply.github.com>
2026-05-03 13:58:04 -0400

    Update submission-2.py
    some explanations and reasoning

--- Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -20,4 +20,14 @@ class Solution:
                 break
 
         return result[:k]
+
+
+# Time Complexity: O(n)
+        
+# Why did we use range(len(nums), 0, -1) instead of range(max(ccounts.keys()), 0, -1) in line 16 ?
+# Ans: Max possible frequency of any element = n (array size) since there are chances when all the elements in the array is the same element.
+#      eg. think of this array [1,1,1,1] where n=4. counts = {1:4}, ccounts={4:1}
+#      Computing max() in terms of time complexity costs O(n) time.
+#      len(nums) → O(1) to compute, max(ccounts.keys()) → O(n) to compute.
+#      Both are valid upper bounds since max frequency ≤ n. Why pay O(n) extra when O(1) gives you the same correct upper bound. So len() was chosen.
             
