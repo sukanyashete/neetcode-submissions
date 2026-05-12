2dfa00a251b8ef764de2ed81113518d19f5c37dc
Sukanya Sudhir Shete <47360797+sukanyashete@users.noreply.github.com>
2026-05-11 23:34:51 -0400

    Bulk sync: 28 submissions

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        left = 0
+        maxlen = 0
+        max_freq = 0
+        freq = defaultdict(int)
+        
+        for right in range(len(s)):
+            # include current char in window
+            freq[s[right]] += 1
+            max_freq = max(max_freq, freq[s[right]])
+            
+            # current window size
+            window_size = right - left + 1
+            
+            # replacements needed = non-max chars in window
+            if window_size - max_freq > k:
+                # shrink from left
+                freq[s[left]] -= 1
+                left += 1
+                window_size = right - left + 1  # optional, since maxlen uses this implicitly
+            
+            maxlen = max(maxlen, right - left + 1)
+        
+        return maxlen
