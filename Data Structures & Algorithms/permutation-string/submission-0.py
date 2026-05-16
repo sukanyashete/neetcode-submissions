bb659294ab33aabb7b15851e86dca1b062650269
Sukanya Sudhir Shete <47360797+sukanyashete@users.noreply.github.com>
2026-05-15 23:29:16 -0400

    Update submission-0.py

--- Data Structures & Algorithms/permutation-string/submission-0.py
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -28,3 +28,6 @@ class Solution:
 
 # Sliding window fixed sized.
 # ord(letter) - ord('a') is done to fit in alphabetical range from 0 to 25 in an array. (a=0, b=1, c=2,.... )
+# ord(letter) : this function gives the ascii value of that letter. 
+# the opposite of this function is chr(letter). Converts the number to letter.
+# chr(num + ord('a')) : Applicable to this case will convert number in range 0-25 to its equivalent a-z.
